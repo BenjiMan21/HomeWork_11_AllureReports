5ed8a811a941c891a1d8e9bf542f57016c2bbf44
Alex Nikiforov <unleash211@gmail.com>
2024-02-04 11:14:07 +0400

    changes

--- src/test/java/com/nikiforov/AllureReportsWithWebStepsTest.java
+++ src/test/java/com/nikiforov/AllureReportsWithWebStepsTest.java
@@ -5,7 +5,6 @@ import io.qameta.allure.*;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import pages.WebSteps;
 
 
 public class AllureReportsWithWebStepsTest {
